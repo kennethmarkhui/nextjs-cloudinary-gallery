4c136c8d9334dbd4f049d6ce38c92a53679de90c
kennethmarkhui <kennethmarkhui@gmail.com>
2022-01-19 12:08:40 +0800

    added blur placeholder for image

--- components/Image/ImageList.js
+++ components/Image/ImageList.js
@@ -2,23 +2,34 @@ import Image from "next/image";
 
 const ImageList = (props) => {
   const { files } = props;
-
   return (
     <>
       <ul>
-        {files.map((item) => (
-          <li key={item.public_id}>
-            {item.filename}
-            <p>{item.secure_url}</p>
-            <Image
-              // unoptimized // sometimes returns 403 forbidden if not set to true
-              src={item.secure_url}
-              alt={item.display_name}
-              width={100}
-              height={100}
-            />
-          </li>
-        ))}
+        {files.map((item) => {
+          const transformations = "e_blur:2000,q_1";
+          let a = item.secure_url;
+          let b;
+
+          return (
+            <li key={item.public_id}>
+              {item.filename}
+              <p>{item.secure_url}</p>
+              <Image
+                // unoptimized // sometimes returns 403 forbidden if not set to true
+                src={item.secure_url}
+                alt={item.display_name}
+                width={100}
+                height={100}
+                placeholder="blur"
+                blurDataURL={
+                  (a =
+                    ((b = a.split("/")).splice(6, 0, transformations),
+                    b.join("/")))
+                }
+              />
+            </li>
+          );
+        })}
       </ul>
     </>
   );
